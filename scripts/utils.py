994efb8db3ec26156e19a21dab52dac992a8d325
Salar Nouri <salr.nouri@gmail.com>
2024-05-05 22:27:03 +0330

    Add noise threshold extract to utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,3 +10,15 @@ def compute_velocity_consecutive(x_position, y_position, timestamp):
     return x_velocity, y_velocity, Pythagorean_velocity
 
 
+def noise_threshold_extract(data_input):
+    # Prefer to lower the values to about 20 deg/sec if the idea is to detect microsaccades
+    threshold = 10
+    idx = (data_input < threshold)
+    gmm = mixture.GaussianMixture(n_components=2, 
+                                  covariance_type='full',
+                                  max_iter=100).fit(data_input[idx].reshape(-1, 1))    
+    means = gmm.means_
+    covariances = np.sqrt(gmm.covariances_)
+    idd = np.argsort(means.reshape(2))
+    value = float((means[idd[0]]+3*covariances[idd[0]])[0])
+    return value
