566cbf461aefd3ea066523f39fe49fad1cceb468
Salar Nouri <salar.nouri@gmail.com>
2024-09-22 17:57:58 +0300

    Modify saccade names in microsaccade detection algorithm

--- scripts/detect_microsaccades.py
+++ scripts/detect_microsaccades.py
@@ -6,19 +6,19 @@ from scripts.utils import (compute_amplitude, compute_partial_velocity,
 
 def detect_microsaccades(x_positions, y_positions, timestamps, params):
     """
-    Detect saccades in eye movement data based on changes in gaze direction.
+    Detect microsaccades in eye movement data based on changes in gaze direction.
     
     Args:
     - x_positions (array): X positions of eye movements.
     - y_positions (array): Y positions of eye movements.
     - timestamps (array): Timestamps corresponding to the eye movement data.
     - params (dict): A dictionary containing:
-        - 'min_duration': Minimum duration (in seconds) required for a valid saccade.
+        - 'min_duration': Minimum duration (in seconds) required for a valid microsaccade.
         - 'threshold_multiplier': Multiplier for sigma_vx and sigma_vy to set velocity threshold.
         - 'sampling_rate': The sampling rate for interpolation (optional).
     
     Returns:
-    - list: List of dictionaries containing information about detected saccades.
+    - list: List of dictionaries containing information about detected microsaccades.
     """
     velocities, sigma_vx, sigma_vy = compute_velocity(x_positions, y_positions, timestamps)
     # velocity_threshold = params['threshold_multiplier'] * np.sqrt(sigma_vx**2 + sigma_vy**2)
@@ -28,7 +28,7 @@ def detect_microsaccades(x_positions, y_positions, timestamps, params):
     
     microsaccades = []
     current_saccade = None
-    saccade_start = None
+    microsaccade_start = None
     
     for i in range(1, len(velocities) - 1):
         
@@ -38,7 +38,7 @@ def detect_microsaccades(x_positions, y_positions, timestamps, params):
         # if velocities[i] >= velocity_threshold:
         if velocity_xy > 1:
             if current_saccade is None:
-                saccade_start = i
+                microsaccade_start = i
                 current_saccade = {
                     'start': i, 
                     'end': i, 
@@ -49,10 +49,10 @@ def detect_microsaccades(x_positions, y_positions, timestamps, params):
             current_saccade['end'] = i
         
         if current_saccade is not None:
-            current_saccade['duration'] = timestamps[current_saccade['end']] - timestamps[saccade_start]
+            current_saccade['duration'] = timestamps[current_saccade['end']] - timestamps[microsaccade_start]
         
             if current_saccade['duration'] >= params['min_duration']:
-                current_saccade['amplitude'] = compute_amplitude(x_positions, y_positions, saccade_start, current_saccade['end'])
+                current_saccade['amplitude'] = compute_amplitude(x_positions, y_positions, microsaccade_start, current_saccade['end'])
                 current_saccade['velocity'] = current_saccade['amplitude'] / current_saccade['duration']
                 # current_saccade['velocity'] = velocities[i]
                 if current_saccade['amplitude'] <= params["amplitude_th"]:
