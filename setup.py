4ea2640e9e30d7024fd6a3e13c85bacfcd3dcf88
Salar Nouri <salar.nouri@gmail.com>
2025-06-12 10:31:52 +0300

    feat: add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ms-selectivity-feature",
+    version="0.1.0",
+    description="Microsaccade Selectivity as Discriminative Feature for Object Decoding",
+    author="Salar Nouri",
+    author_email="salar.nouri@epfl.ch",
+    url="https://github.com/sa-nouri/ms-selectivity-feature",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=2.1.1",
+        "scikit-learn>=1.5.2",
+        "scipy>=1.14.1",
+        "matplotlib>=3.9.2",
+        "pandas>=2.2.3",
+        "seaborn>=0.13.2",
+    ],
+    python_requires=">=3.11",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+) 
